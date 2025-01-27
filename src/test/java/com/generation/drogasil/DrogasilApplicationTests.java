b7e614b501cf069ce4de39400f03e79356ce18a1
Jessica Gomes <J.gomesss@outlook.com>
2025-01-27 10:42:59 -0300

    Configurações

---
+++ src/test/java/com/generation/drogasil/DrogasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.drogasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
